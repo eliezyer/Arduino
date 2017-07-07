5218ae2510f4838f1b0a3a6c5c334c1240d85d27
eliezyer <fermino.bioeng@gmail.com>
2017-07-06 20:05:33 -0400

    Add files via upload

--- DelayAlternating.ino
+++ DelayAlternating.ino
@@ -11,6 +11,8 @@ int SMState = 0;
 int SRState = 0;
 int previousArm = 0; //0 is left and 1 is right arm, variable to save the previous arm visited.
 int correctArm; //0 is left and 1 is right arm, variable to save the correct Arm animal has to go.
+int correctTrials = 0;
+int wrongTrials = 0;
 
 void setup()
 {
@@ -40,8 +42,6 @@ void loop()
   if (SMState == LOW && flagEnd == 1) //this will be important to send the TTLs to the recording system, about the beggining of the trials
   {
       Trial++;
-      Serial.println(Trial);
-      delay(1000);
       flagEnd = 0;
       flagStart = 1;
   }
@@ -51,24 +51,24 @@ if (flagStart == 1)
   if (SLState == LOW && correctArm == 0)
   {
       //opens the left door and delivery the water at the left
-      Serial.println("Correct!");
-      digitalWrite(ledPin,HIGH);
       delay(1000);
-      digitalWrite(ledPin,LOW);
       
       previousArm = correctArm;
       flagEnd = 1;
       flagStart = 0;
+      correctTrials++;
   }
-  else
+  else if (SLState == LOW && correctArm == 1)
   {
-    digitalWrite(ledPin,LOW);
+      delay(1000);
+      flagEnd = 1;
+      flagStart = 0;
+      wrongTrials++;
   }
   
   if (SRState == LOW && correctArm == 1)
   {
     //open the right door and delivery the water at the right
-      Serial.println("Correct!");
       digitalWrite(ledPin,HIGH);
       delay(1000);
       digitalWrite(ledPin,LOW);
@@ -76,6 +76,14 @@ if (flagStart == 1)
       previousArm = correctArm;
       flagEnd = 1;
       flagStart = 0;
+      correctTrials++;
+  }
+  else if (SRState == LOW && correctArm == 0)
+  {
+      delay(1000);
+      flagEnd = 1;
+      flagStart = 0;
+      wrongTrials++;
   }
 }
 
@@ -83,11 +91,27 @@ if (flagStart == 1)
   //just to check at the serial monitor
   if (correctArm == 0)
   {
-    Serial.println("Left Arm");
+    Serial.print("Left Arm  - ");
+    Serial.print("Trial: ");
+    Serial.print(Trial);
+    Serial.print(" Correct: ");
+    Serial.print(correctTrials);
+    Serial.print(" Wrong: ");
+    Serial.print(wrongTrials);
+    Serial.print(" Score (%): ");
+    Serial.println(correctTrials*100/Trial);
   }
   else if (correctArm == 1)
   {
-    Serial.println("Right Arm");
+    Serial.print("Right Arm  - ");
+    Serial.print("Trial: ");
+    Serial.print(Trial);
+    Serial.print(" Correct: ");
+    Serial.print(correctTrials);
+    Serial.print(" Wrong: ");
+    Serial.print(wrongTrials);
+    Serial.print(" Score (%): ");
+    Serial.println(correctTrials*100/Trial);
   }
   
 
